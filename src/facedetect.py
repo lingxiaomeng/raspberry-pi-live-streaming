a8bb1530a69961c65039f3330d75cef5c182b9d1
lingxiaomeng <574243996@qq.com>
2019-05-07 10:01:04 +0800

    update

---
+++ src/facedetect.py
@@ -0,0 +1,28 @@
+import time
+import cv2
+import numpy as np
+
+detector = cv2.CascadeClassifier('eyedetect.xml')
+cap = cv2.VideoCapture(0)
+
+
+while True:
+
+    cv_image, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = detector.detectMultiScale(gray, 1.3, 5)
+    if len(faces) >= 1:
+        print("发现{0}个人脸!".format(len(faces)))
+
+    for (x, y, w, h) in faces:
+        # cv2.rectangle(image,(x,y),(x+w,y+w),(0,255,0),2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    cv2.imshow('frame', frame)
+    # pygame.image.save(image, "pygame1.jpg")
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
